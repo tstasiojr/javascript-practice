c5e714305447fcf6f8fec9f95f81c38c859740b1
Terry Stasio <probasix@gmail.com>
2021-06-01 21:03:24 -0700

    added length and BigInts

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,16 @@ comment */
 
 console.log('Terry'); // logging my name to the console
 console.log('Pizza'); // this is my favorite food
+
+const docious = "Supercalifragilisticexpialidocious";
+console.log('string length', docious.length);
+
+console.log(("Supercalifragilisticexpialidocious").length);
+
+console.log(53);
+console.log(typeof 53);
+
+console.log(65465465460546546540654064065460n);
+console.log(BigInt("65465465460546546540654064065460"));
+
+console.log(typeof 65465465460546546540654064065460n)
