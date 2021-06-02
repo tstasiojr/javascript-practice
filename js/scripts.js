75027f8697c275aa615744c1ba311a2b04720594
Terry Stasio <probasix@gmail.com>
2021-06-01 20:16:12 -0700

    added comments and console commands

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// This is a single line comment
+
+/* This
+is
+a
+multiline
+comment */
+
+console.log('Terry'); // logging my name to the console
+console.log('Pizza'); // this is my favorite food
